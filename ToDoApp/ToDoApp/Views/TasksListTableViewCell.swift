9c968053e492160a3e94bf2596402df1b3dd593f
gaida <gam.5kk@gmail.com>
2020-11-03 14:00:35 +0300

    add editing feature

---
+++ ToDoApp/ToDoApp/Views/TasksListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TasksListTableViewCell.swift
+//  ToDoApp
+//
+//  Created by Gaida  on 03/11/2020.
+//
+
+import UIKit
+
+class TasksListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
